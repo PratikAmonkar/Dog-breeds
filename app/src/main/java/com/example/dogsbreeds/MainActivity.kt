fdf58c378d1f5a59232efee976bbd9adeb2823e5
Pratik Amonkar <amonkar3000@gmail.com>
2023-11-13 18:46:45 +0530

    home screen ui created

--- app/src/main/java/com/example/dogsbreeds/MainActivity.kt
+++ app/src/main/java/com/example/dogsbreeds/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.dogsbreeds.ui.screens.HomeScreen
 import com.example.dogsbreeds.ui.screens.LoginScreen
 import com.example.dogsbreeds.ui.screens.SignUpScreen
 import com.example.dogsbreeds.ui.theme.DogsBreedsTheme
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen()
+                    HomeScreen()
                 }
             }
         }
--- app/src/main/java/com/example/dogsbreeds/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/dogsbreeds/ui/screens/HomeScreen.kt
@@ -1,20 +1,15 @@
 package com.example.dogsbreeds.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.dogsbreeds.R
 import com.example.dogsbreeds.ui.composables.AppBar
 import com.example.dogsbreeds.ui.composables.DisplayDogBreed
 
