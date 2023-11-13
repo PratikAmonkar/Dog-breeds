fdd4249a97a2da67f8625ac7a2d9d3efce62e7a8
Pratik Amonkar <amonkar3000@gmail.com>
2023-11-13 18:26:32 +0530

    login screen ui created

--- app/src/main/java/com/example/dogsbreeds/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/dogsbreeds/ui/screens/LoginScreen.kt
@@ -7,9 +7,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,7 +28,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dogsbreeds.R
 import com.example.dogsbreeds.ui.composables.AppBar
 import com.example.dogsbreeds.ui.composables.CustomButton
@@ -30,7 +42,7 @@ import com.example.dogsbreeds.ui.composables.CustomButton
 @Composable
 fun LoginScreen() {
 
-    var userEmail by rememberSaveable {
+    var userName by rememberSaveable {
         mutableStateOf("")
     }
     var userPassword by rememberSaveable {
@@ -47,35 +59,73 @@ fun LoginScreen() {
                 .fillMaxSize()
                 .verticalScroll(
                     rememberScrollState(),
-
                     ),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Image(
                 painter = painterResource(id = R.drawable.login_img),
-                contentDescription = "Login image"
+                contentDescription = "Login image",
+                modifier = Modifier.size(300.dp)
             )
             OutlinedTextField(
-                value = userEmail,
+                value = userName,
                 onValueChange = { value ->
-                    userEmail = value
+                    userName = value
                 },
+                placeholder = {
+                    Text(
+                        "Enter username",
+                        style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.W500)
+                    )
+                },
+                textStyle = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.W500),
+                leadingIcon = {
+                    Icon(imageVector = Icons.Filled.Person, contentDescription = "Person icon")
+                },
+                keyboardOptions = KeyboardOptions(
+                    autoCorrect = false,
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {}
+                ),
+                singleLine = true,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(16.dp),
             )
             OutlinedTextField(
                 value = userPassword,
                 onValueChange = { value ->
                     userPassword = value
                 },
+                placeholder = {
+                    Text(
+                        "Enter password",
+                        style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.W500)
+                    )
+                },
+                textStyle = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.W500),
+                leadingIcon = {
+                    Icon(imageVector = Icons.Filled.Lock, contentDescription = "Password icon")
+                },
+                keyboardOptions = KeyboardOptions(
+                    autoCorrect = false,
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {}
+                ),
+                singleLine = true,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(16.dp),
             )
 
             CustomButton(title = "Login", action = {})
-            Text("OR")
+            Text("OR", style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.W400))
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -83,8 +133,11 @@ fun LoginScreen() {
                 horizontalArrangement = Arrangement.Center
             )
             {
-                Text("Don't have an account ? ")
-                Text("Register ")
+                Text(
+                    "Don't have an account ? ",
+                    style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.W400)
+                )
+                Text("Register ", style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.W400))
             }
         }
     }
