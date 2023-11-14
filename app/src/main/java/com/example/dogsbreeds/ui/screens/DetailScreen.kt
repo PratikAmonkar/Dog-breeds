359cbd1ea5e2bdaa783d858813f0354ad251a14d
Pratik Amonkar <amonkar3000@gmail.com>
2023-11-14 12:17:53 +0530

    fix issue of image

--- app/src/main/java/com/example/dogsbreeds/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/example/dogsbreeds/ui/screens/DetailScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -20,6 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -77,26 +80,36 @@ fun DetailScreen(
                                 rememberScrollState(),
                             ),
                     ) {
-                        SubcomposeAsyncImage(
-                            model = ImageRequest.Builder(context = LocalContext.current)
-                                .data(successState.listData.url).crossfade(true).build(),
-                            contentDescription = "dog image",
-                            loading = {
-                                Column(
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                    verticalArrangement = Arrangement.Center
-                                ) {
-                                    Box(modifier = Modifier.size(50.dp)) {
-                                        CircularProgressIndicator()
-                                    }
-                                }
-                            },
+                        Box(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(10.dp)
-                                .clip(RoundedCornerShape(20.dp)),
-                            alignment = Alignment.Center,
-                        )
+                                .height(300.dp)
+                                .clip(RoundedCornerShape(16.dp))
+                                .wrapContentSize()
+                        ) {
+                            SubcomposeAsyncImage(
+                                model = ImageRequest.Builder(context = LocalContext.current)
+                                    .data(successState.listData.url).crossfade(true).build(),
+                                contentDescription = "dog image",
+                                loading = {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                        verticalArrangement = Arrangement.Center
+                                    ) {
+                                        Box(modifier = Modifier.size(50.dp)) {
+                                            CircularProgressIndicator()
+                                        }
+                                    }
+                                },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(10.dp)
+                                    .clip(RoundedCornerShape(20.dp)),
+                                contentScale = ContentScale.Crop,
+                                alignment = Alignment.Center,
+                            )
+                        }
+
                         Text(
                             item.name,
                             style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp),
@@ -108,7 +121,7 @@ fun DetailScreen(
                             text = item.description ?: "",
                             style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
 
-                        )
+                            )
 
                         BreedDetail(
                             attribute = "Breed Group",
